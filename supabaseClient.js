c09f538f8562928eb0ed6f4c470a34f5c417f7a3
beatrizgomes170518 <160758165+beatrizgomes170518@users.noreply.github.com>
2025-05-13 08:02:35 -0300

    kjh

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,5 +4,4 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://wsbjkdyzwxyxbnepzzjk.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzYmprZHl6d3h5eGJuZXB6emprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1NjgsImV4cCI6MjA2MjExMTU2OH0.RznHPwAgXnkr1yWnNvbsaoVq_1X_LebWr_2Pf5Ua56s'
-)
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzYmprZHl6d3h5eGJuZXB6emprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1NjgsImV4cCI6MjA2MjExMTU2OH0.RznHPwAgXnkr1yWnNvbsaoVq_1X_LebWr_2Pf5Ua56s')
